2e15c0816f689c62d75fcf5748e5cf07b429ddb7
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-07-12 14:24:10 +0100

    feat: screens styles

---
+++ src/screens/search/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import * as colors from "../../common/colors";
+
+export const Searchstyles = StyleSheet.create({
+  searchbox: {
+    marginTop: 80,
+    paddingHorizontal: 30,
+  },
+  search: {
+    fontSize: 18,
+  },
+});
---
+++ src/screens/website/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+import * as colors from "../../common/colors";
+
+export const Websitestyles = StyleSheet.create({
+  webcnot: {
+    marginTop: 80,
+    paddingHorizontal: 30,
+  },
+});
